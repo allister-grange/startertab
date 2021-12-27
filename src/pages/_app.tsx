7e384de9ce5003151c8f175f096d290628f0b4fc
Allister Grange <allistergrange@gmail.com>
2021-12-27 15:47:05 +1300

    Starting webpage with dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, ThemeConfig } from "@chakra-ui/react";
 
-// 2. Extend the theme to include custom colors, fonts, etc
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
+const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: false,
-};
+}
 
-const theme = extendTheme({ colors });
+const theme = extendTheme({ config });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
