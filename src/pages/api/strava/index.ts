68ba868f74a013ce96e896383e46d72c4b09b95f
Allister Grange <allistergrange@gmail.com>
2022-06-25 20:42:57 +1200

    Fixing timezone issues with Strava's API

--- src/pages/api/strava/index.ts
+++ src/pages/api/strava/index.ts
@@ -91,36 +91,32 @@ const formatStravaData = (data: StravaActivity[]) => {
   const formattedStravaData = getEmptyStravaData();
 
   data.forEach((activity) => {
+    const utcDate = new Date(activity.start_date);
+    const localDate = new Date(utcDate);
+
     if (activity.type === "Run") {
       formattedStravaData.running.forEach((activityToFind) => {
-        if (
-          activityToFind.day === weekday[new Date(activity.start_date).getDay()]
-        ) {
-          activityToFind.distance =
+        if (activityToFind.day === weekday[localDate.getDay()]) {
+          activityToFind.distance +=
             Math.round((activity.distance / 1000) * 10) / 10;
         }
       });
       formattedStravaData.combinedData.forEach((activityToFind) => {
-        if (
-          activityToFind.day === weekday[new Date(activity.start_date).getDay()]
-        ) {
-          activityToFind.run = Math.round((activity.distance / 1000) * 10) / 10;
+        if (activityToFind.day === weekday[localDate.getDay()]) {
+          activityToFind.run +=
+            Math.round((activity.distance / 1000) * 10) / 10;
         }
       });
     } else if (activity.type === "Swim") {
       formattedStravaData.swimming.forEach((activityToFind) => {
-        if (
-          activityToFind.day === weekday[new Date(activity.start_date).getDay()]
-        ) {
-          activityToFind.distance =
+        if (activityToFind.day === weekday[localDate.getDay()]) {
+          activityToFind.distance +=
             Math.round((activity.distance / 1000) * 10) / 10;
         }
       });
       formattedStravaData.combinedData.forEach((activityToFind) => {
-        if (
-          activityToFind.day === weekday[new Date(activity.start_date).getDay()]
-        ) {
-          activityToFind.swim =
+        if (activityToFind.day === weekday[localDate.getDay()]) {
+          activityToFind.swim +=
             Math.round((activity.distance / 1000) * 10) / 10;
         }
       });
