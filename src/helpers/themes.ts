d75325485daf82aa4612085fc05de25e798212f2
Allister Grange <allistergrange@gmail.com>
2022-07-15 15:55:13 +1200

    Removing Strava from the default options for themes + adding in pink theme

--- src/helpers/themes.ts
+++ src/helpers/themes.ts
@@ -14,7 +14,7 @@ export const defaultSettings: UserSettings = {
         colorPrimary: "#33393D",
         colorSecondary: "#666",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "#65abc1",
@@ -24,7 +24,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "#E89C4B",
         textColor: "#222222",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "#9AB899",
@@ -84,7 +84,7 @@ export const defaultSettings: UserSettings = {
         tileBorder: "",
         colorPrimary: "#eeeeee",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "#65abc1",
@@ -94,7 +94,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "#E89C4B",
         textColor: "#ffffff",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "#9AB899",
@@ -154,7 +154,7 @@ export const defaultSettings: UserSettings = {
         tileBorder: "1px solid black",
         colorPrimary: "#eeeeee",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "#ffffff",
@@ -164,7 +164,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "#ffffff",
         textColor: "#222222",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "#ffffff",
@@ -225,7 +225,7 @@ export const defaultSettings: UserSettings = {
         colorSecondary: "#666",
         dropShadow: "2px 2px 6px rgba(0,0,0,.3)",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "#222222",
@@ -235,7 +235,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "#222222",
         textColor: "#E0E0E0",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "#222222",
@@ -288,14 +288,15 @@ export const defaultSettings: UserSettings = {
     {
       themeName: "glassmorphism light",
       globalSettings: {
-        backgroundColor: "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%) fixed",
+        backgroundColor:
+          "linear-gradient(160deg, #0093E9 0%, #80D0C7 100%) fixed",
         textColor: "#222222",
         tileType: "None",
         dropShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
         tileBorder: "1px solid rgba(255, 255, 255, 0.3)",
         colorPrimary: "#eeeeee",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
@@ -305,7 +306,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
         textColor: "#222222",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
@@ -358,7 +359,8 @@ export const defaultSettings: UserSettings = {
     {
       themeName: "glassmorphism dark",
       globalSettings: {
-        backgroundColor: "linear-gradient(160deg, black 0%, #80D0C7 100%) fixed",
+        backgroundColor:
+          "linear-gradient(160deg, black 0%, #80D0C7 100%) fixed",
         textColor: "#fff",
         tileType: "None",
         dropShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
@@ -367,7 +369,7 @@ export const defaultSettings: UserSettings = {
         subTextColor: "#ddd",
         colorSecondary: "#666",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
@@ -377,7 +379,78 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
         textColor: "#222222",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
+      },
+      tile3: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Todo List",
+      },
+      tile4: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Reddit Feed",
+      },
+      tile5: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Search Bar",
+      },
+      tile6: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Bonsai",
+        bonsaiTrunkColor: "#202020",
+        bonsaiBaseColor: "#000",
+      },
+      tile7: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Small Weather Tile",
+      },
+      tile8: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Small Stock Tile",
+      },
+      tile9: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Large Spotify Tile",
+      },
+      tile10: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Time",
+      },
+      tile11: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Theme Picker",
+      },
+    },
+    {
+      themeName: "Pink",
+      globalSettings: {
+        backgroundColor:
+          "linear-gradient(to bottom right, #ABA1EE , #F29AD8) fixed",
+        textColor: "#222222",
+        tileType: "None",
+        dropShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+        tileBorder: "1px solid rgba(255, 255, 255, 0.3)",
+        colorPrimary: "#eeeeee",
+        borderRadius: "15px",
+        gridGap: "16px",
+      },
+      tile1: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Hacker News Feed",
+      },
+      tile2: {
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        textColor: "#222222",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
@@ -437,7 +510,7 @@ export const defaultSettings: UserSettings = {
         tileBorder: "1px solid rgba(255, 255, 255, 0.3)",
         colorPrimary: "#eeeeee",
         borderRadius: "15px",
-        gridGap: "16px"
+        gridGap: "16px",
       },
       tile1: {
         backgroundColor: "#81F8FF",
@@ -447,7 +520,7 @@ export const defaultSettings: UserSettings = {
       tile2: {
         backgroundColor: "rgba(255, 255, 255, 0.2)",
         textColor: "#222222",
-        tileType: "Strava Graph",
+        tileType: "Large Weather Tile",
       },
       tile3: {
         backgroundColor: "#ffff82",
