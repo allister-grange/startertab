eb1e8b2fd58558dca95439d16f0c5106053473ca
Allister Grange <allistergrange@gmail.com>
2022-11-27 14:25:28 +1300

    Tidying up the footer contents

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,40 +1,57 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Link from "next/link";
 import React from "react";
+import { GithubSvg } from "./GithubSvg";
+import { OutlinedButton } from "./OutlinedButton";
 
 export const Footer: React.FC = ({}) => {
   return (
-    <Box background="#F7F8FA" height="150px" display="fixed" color="gray.900">
+    <Box background="white" height="150px" display="fixed" color="gray.900">
       <Flex
         height="100%"
-        width={["100%", "100%", "90%", "70%"]}
+        width="100%"
         marginX="auto"
-        p="6"
+        px="2"
         alignItems="center"
         justifyContent="space-between"
+        flexWrap="wrap"
       >
         <Box>
           <Heading fontSize={["lg", "lg", "2xl"]} as="h3">
             Starter Tab
           </Heading>
           <Heading
-            fontSize={["md", "md", "lg"]}
+            fontSize={["md", "md", "md"]}
             mt="2"
             as="h3"
             color="gray.600"
           >
-            © whatever year it is lol; all rights reserved
+            ©2022; all rights reserved
           </Heading>
+          <Text fontSize="sm" width="70%" mt="2">
+            <Link href="mailto:allistergrange@gmail.com">
+              feedback is invaluable
+            </Link>
+          </Text>
         </Box>
         <Box>
-          <Box>
-            <Link href="emailto:allistergrange@gmail.com">Email me</Link>
-          </Box>
-          <Box mt="2">
-            <Link href="https://github.com/allister-grange/startertab">
-              Code for this site
+          <Flex alignItems="center">
+            <Box w="2" h="2" bg="lightgreen" borderRadius="100%" />
+            <Box ml="2" />
+            <Link
+              href="mailto:allistergrange@gmail.com"
+              style={{ marginLeft: "2" }}
+            >
+              allig256
             </Link>
-          </Box>
+          </Flex>
+          <Flex alignItems="center" mt="2">
+            <GithubSvg height={16} width={16} fill={"black"} />
+            <Box ml="2" />
+            <Link href={"https://github.com/allister-grange/startertab"}>
+              code
+            </Link>
+          </Flex>
         </Box>
       </Flex>
     </Box>
