b991eb90040ca88251ccaee9dc6b4cd422f9113e
Allister Grange <allistergrange@gmail.com>
2023-01-28 11:55:09 +1300

    Updating the 404 page to work regardless of theme

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,15 @@
 import { Center, Heading, Link } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 
 const Custom404: React.FC = ({}) => {
+  useEffect(() => {
+    document.body.style.background = "white";
+  }, []);
+
   return (
-    <Center height="90%" flexDir="column">
+    <Center height="90%" flexDir="column" color="black">
       <Heading>Page not found 🤕</Heading>
-      <Link fontSize="md" mt="2" href="/">
+      <Link fontSize="md" mt="2" href="/" color="gray.700">
         Take me home &larr;
       </Link>
     </Center>
