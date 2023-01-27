193d2a777bd055a4656a423d9ae7580df2e8e53d
Allister Grange <allistergrange@gmail.com>
2023-01-27 16:24:14 +1300

    Adding in legal jargon

--- src/components/sidebar/Footer.tsx
+++ src/components/sidebar/Footer.tsx
@@ -1,6 +1,6 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
-import { GithubSvg } from "@/components/ui/GithubSvg";
+import { GithubSvg } from "@/components/icons/GithubSvg";
 import Link from "next/link";
 
 interface SideBarFooterProps {
