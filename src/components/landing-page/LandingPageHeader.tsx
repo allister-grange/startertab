1ffc994b19fe172e6fa1b380b4950ab1f13ddb8b
Allister Grange <allistergrange@gmail.com>
2023-02-10 19:20:32 +1300

    Adding in an hamburger to the header

--- src/components/landing-page/LandingPageHeader.tsx
+++ src/components/landing-page/LandingPageHeader.tsx
@@ -1,35 +1,84 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
-import React from "react";
 import { StarterTabLogo } from "@/components/ui/StarterTabLogo";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  HStack,
+  IconButton,
+  Link as ChakraLink,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  useMediaQuery,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import React from "react";
+import { OutlinedButton } from "@/components/ui/OutlinedButton";
 
 export const Header: React.FC = () => {
+  const [isSmallScreen] = useMediaQuery("(max-width: 900px)");
+
   return (
     <Box bg="white" width="100%">
       <Flex
         as="nav"
         align="center"
         justify="space-between"
-        wrap="wrap"
         bg="rgba(255,255,255,0.72)"
         backdropFilter="saturate(180%) blur(20px) !important"
-        padding="1rem"
+        padding="1"
         maxWidth="1200px"
         margin="0 auto"
+        pt="6"
+        px={[4, 6, 8, 12]}
         color="black"
       >
         <Flex align="center" mr={5}>
           <StarterTabLogo />
         </Flex>
-        <Flex justify="flex-end" align="center">
-          <Link
-            display="block"
-            href="/"
-            color="gray.700"
-            fontSize={["sm", "unset"]}
-          >
-            Take me to the app 👉
-          </Link>
-        </Flex>
+        <HStack columnGap="5" justify="flex-end" align="center">
+          {!isSmallScreen ? (
+            <>
+              <ChakraLink href="/themes#public" fontWeight="600">
+                Themes
+              </ChakraLink>
+              <ChakraLink href="/updates" fontWeight="600">
+                Updates
+              </ChakraLink>
+              <OutlinedButton
+                borderColor="coral"
+                onClick={() => (window.location.href = "/")}
+              >
+                <Link href="/">Take me to the app 👉</Link>
+              </OutlinedButton>
+            </>
+          ) : (
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label="Options"
+                icon={<HamburgerIcon />}
+                variant="outline"
+                borderColor="coral"
+                color="black"
+              />
+              <MenuList>
+                <MenuItem
+                  onClick={() => (window.location.href = "/themes#public")}
+                >
+                  Themes
+                </MenuItem>
+                <MenuItem onClick={() => (window.location.href = "/updates")}>
+                  Updates
+                </MenuItem>
+                <MenuItem onClick={() => (window.location.href = "/")}>
+                  Take me to the app 👉
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          )}
+        </HStack>
       </Flex>
     </Box>
   );
