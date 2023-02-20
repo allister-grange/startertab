91ec82bb720558c10ef67f24c1d14e80d74ef5b7
Allister Grange <allistergrange@gmail.com>
2023-02-21 12:37:08 +1300

    Turning off Twitter Tile 😢

--- src/components/tiles/TwitterFeedTile.tsx
+++ src/components/tiles/TwitterFeedTile.tsx
@@ -20,13 +20,14 @@ interface TwitterFeedTileProps {
   tileId: number;
 }
 
+// DISABLED UNTIL TWITTER API IS BACK TO BEING FREE
 export const TwitterFeedTile: React.FC<TwitterFeedTileProps> = ({ tileId }) => {
   const color = `var(--text-color-${tileId})`;
 
-  const { isAuthenticated, twitterData, loginWithTwitter, isLoading, error } =
-    useContext(TwitterContext) as TwitterContextInterface;
   const [displayingOnWideTile, setDisplayingOnWideTile] = useState(false);
   const divRef = useRef<HTMLDivElement | null>(null);
+  // const { isAuthenticated, twitterData, loginWithTwitter, isLoading, error } =
+  //   useContext(TwitterContext) as TwitterContextInterface;
 
   // need to change the amount of text truncated from title depending on width
   React.useEffect(() => {
@@ -39,69 +40,69 @@ export const TwitterFeedTile: React.FC<TwitterFeedTileProps> = ({ tileId }) => {
     }
   }, []);
 
-  if (isAuthenticated === false) {
-    return (
-      <Center height="100%">
-        <OutlinedButton
-          onClick={loginWithTwitter}
-          color={color}
-          borderColor={color}
-        >
-          Continue with Twitter&nbsp;
-          <TwitterLogo fill="#1E9CEA" height={20} width={20} />
-        </OutlinedButton>
-      </Center>
-    );
-  }
+  // if (isAuthenticated === false) {
+  //   return (
+  //     <Center height="100%">
+  //       <OutlinedButton
+  //         onClick={loginWithTwitter}
+  //         color={color}
+  //         borderColor={color}
+  //       >
+  //         Continue with Twitter&nbsp;
+  //         <TwitterLogo fill="#1E9CEA" height={20} width={20} />
+  //       </OutlinedButton>
+  //     </Center>
+  //   );
+  // }
 
-  let display;
+  // let display;
 
-  if (isLoading && isAuthenticated !== undefined) {
-    display = <TextFeedSkeleton />;
-  } else if (error) {
-    display = (
-      <Box mt="6" textAlign="center" px="2">
-        <Text>There was an error fetching the Twitter data.</Text>
-        <br />
-        <Text>
-          If this error continues to persist, please open a{" "}
-          <Link
-            target="_top"
-            style={{ textDecoration: "underline" }}
-            href="https://github.com/allister-grange/startertab/issues"
-          >
-            GitHub issue
-          </Link>
-          .
-        </Text>
-      </Box>
-    );
-  } else if (twitterData) {
-    display = (
-      <UnorderedList margin="0" mt="4" px="2">
-        {twitterData.map((tweet) => (
-          <ListItem listStyleType="none" key={tweet.id} mt="3">
-            <Link
-              target="_top"
-              href={`https://twitter.com/startertabeasteregg/statuses/${tweet.id}`}
-            >
-              {displayingOnWideTile
-                ? tweet.text
-                : truncateString(tweet.text, 90)}
-            </Link>
-            <Flex justifyContent="space-between">
-              <Text fontSize="xs">
-                {calculateTimeAgoString(new Date(tweet.created_at))}
-              </Text>
-              <Text fontSize="xs">{tweet.author}</Text>
-            </Flex>
-          </ListItem>
-        ))}
-      </UnorderedList>
-    );
-  } else {
-    return <></>;
-  }
+  // if (isLoading && isAuthenticated !== undefined) {
+  //   display = <TextFeedSkeleton />;
+  // } else if (error) {
+  //   display = (
+  //     <Box mt="6" textAlign="center" px="2">
+  //       <Text>There was an error fetching the Twitter data.</Text>
+  //       <br />
+  //       <Text>
+  //         If this error continues to persist, please open a{" "}
+  //         <Link
+  //           target="_top"
+  //           style={{ textDecoration: "underline" }}
+  //           href="https://github.com/allister-grange/startertab/issues"
+  //         >
+  //           GitHub issue
+  //         </Link>
+  //         .
+  //       </Text>
+  //     </Box>
+  //   );
+  // } else if (twitterData) {
+  //   display = (
+  //     <UnorderedList margin="0" mt="4" px="2">
+  //       {twitterData.map((tweet) => (
+  //         <ListItem listStyleType="none" key={tweet.id} mt="3">
+  //           <Link
+  //             target="_top"
+  //             href={`https://twitter.com/startertabeasteregg/statuses/${tweet.id}`}
+  //           >
+  //             {displayingOnWideTile
+  //               ? tweet.text
+  //               : truncateString(tweet.text, 90)}
+  //           </Link>
+  //           <Flex justifyContent="space-between">
+  //             <Text fontSize="xs">
+  //               {calculateTimeAgoString(new Date(tweet.created_at))}
+  //             </Text>
+  //             <Text fontSize="xs">{tweet.author}</Text>
+  //           </Flex>
+  //         </ListItem>
+  //       ))}
+  //     </UnorderedList>
+  //   );
+  // } else {
+  //   return <></>;
+  // }
 
   return (
     <Box p="2" color={color} position="relative" mb="2" ref={divRef}>
@@ -119,9 +120,12 @@ export const TwitterFeedTile: React.FC<TwitterFeedTileProps> = ({ tileId }) => {
       </Heading>
       <Box w="80%" bg="white" height="1px" ml="2" bgColor={color} />
       {/* {display} */}
-      <Center mt="8">
-        You can blame{" "}
-        <a href="https://twitter.com/elonmusk/">&nbsp;@ElonMusk</a>
+      <Center mt="8" flexDir="column" textAlign="center">
+        <p>
+          You can blame
+          <a href="https://twitter.com/elonmusk/">&nbsp;@ElonMusk</a>, the API
+          now costs and StarterTab is a free product
+        </p>
       </Center>
     </Box>
   );
