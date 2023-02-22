c145d0c7277570e70b01a4ade644c878aef6595f
Allister Grange <allistergrange@gmail.com>
2023-02-22 17:33:51 +1300

    Cleaning up the App Error Boundary

--- src/components/ui/AppErrorBoundary.tsx
+++ src/components/ui/AppErrorBoundary.tsx
@@ -1,12 +1,10 @@
 import { OutlinedButton } from "@/components/ui/OutlinedButton";
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import Router from "next/router";
-import React, { useEffect } from "react";
+import React from "react";
 
 export const AppErrorBoundary: React.FC = ({}) => {
-  useEffect(() => {
-    document.body.style.background = "white";
-  }, []);
+  document.body.style.background = "white";
 
   const deleteAllSettings = () => {
     localStorage.clear();
@@ -27,33 +25,47 @@ export const AppErrorBoundary: React.FC = ({}) => {
   return (
     <Box
       height="100%"
-      width={["100%", "90%", "70%", "50%", "50%"]}
+      width={["100%", "90%", "70%", "60%", "60%"]}
       p="2"
       mt="24"
       mx="auto"
-      color="black"
       textAlign="center"
+      color="gray.700"
     >
       <Flex>
         <Box my="auto" width="100%">
-          <Heading color="black" fontSize="40px">
-            I am in a pickle! ⚠️
+          <Heading fontSize="4xl" fontWeight="600" color="gray.600">
+            Looks like something went wrong!
           </Heading>
-          <Text mt="4">
+          <Flex mt="4" textAlign="center" justifyContent="center">
+            <Text fontSize="2xl">
+              Open an issue on{" "}
+              <a
+                href="https://github.com/allister-grange/startertab"
+                target="_black"
+                style={{ textDecoration: "underline" }}
+              >
+                GitHub
+              </a>
+            </Text>
+          </Flex>
+          <Text mt="8" fontSize="xl">
             It seems there is an error that has occurred. Give refreshing your
-            browser a shot. There&apos;s probably an issue with the settings for
+            browser a try. There&apos;s probably an issue with the settings for
             your tile&apos;s.
           </Text>
-          <Text mt="2">
+          <Text mt="4" fontSize="lg">
             If refreshing doesn&apos;t help, please click this dangerous button
-            below &darr; This will delete all of your settings data.
+            below &darr; This will delete all of your data. Please back up your
+            localstorage if you have themes that you care about. In the future,
+            it might be a good idea to publically list themes you care about.
           </Text>
         </Box>
       </Flex>
-      <Box textAlign="center" mt="16px">
+      <Box textAlign="center" mt="8">
         <OutlinedButton
-          borderColor="red"
           fontWeight="800"
+          border="1px solid black"
           onClick={deleteAllSettings}
           px="4"
           background="white"
@@ -63,7 +75,7 @@ export const AppErrorBoundary: React.FC = ({}) => {
           py="6"
           borderRadius="10"
         >
-          CLEAR SETTINGS
+          Clear all settings! ⚠️
         </OutlinedButton>
       </Box>
     </Box>
