10bcf178d21bcdaee17a5030cef064cddd202bfe
Allister Grange <allistergrange@gmail.com>
2024-02-06 16:34:57 +1300

    Getting the timer to work across tabs - what a mission, I hate setInterval

--- src/components/tiles/TimeTile.tsx
+++ src/components/tiles/TimeTile.tsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useRef, useState } from "react";
+import { SetterOrUpdater, useRecoilState, useRecoilValue } from "recoil";
+import { Box, Button, Heading, Input } from "@chakra-ui/react";
 import { OutlinedButton } from "@/components/ui/OutlinedButton";
 import { sidebarOpenAtom } from "@/recoil/SidebarAtoms";
 import {
@@ -5,46 +8,20 @@ import {
   timeTileShowingSecondsSelector,
   timeTileShowingTimerSelector,
 } from "@/recoil/UserSettingsSelectors";
-import { Box, Button, Heading, Input } from "@chakra-ui/react";
-import React, { useEffect, useRef, useState } from "react";
-import { SetterOrUpdater, useRecoilState, useRecoilValue } from "recoil";
 
 interface TimeProps {
   tileId: number;
 }
 
-function getCurrentTime(
-  is12HourFormat: boolean,
-  isShowingSeconds: boolean
-): string {
-  const now = new Date();
+const getTimerDisplayText = (time: number) => {
+  let minutes: any = parseInt((time / 60).toString(), 10);
+  let seconds: any = parseInt((time % 60).toString(), 10);
 
-  let hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-  let timeString = "";
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-  if (is12HourFormat) {
-    // Convert 24 hour time to 12 hour format
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    timeString = `${hours < 10 ? "0" + hours : hours}:${
-      minutes < 10 ? "0" + minutes : minutes
-    }`;
-  } else {
-    // 24 hour format
-    timeString = `${hours < 10 ? "0" + hours : hours}:${
-      minutes < 10 ? "0" + minutes : minutes
-    }`;
-  }
-
-  if (isShowingSeconds) {
-    // Append seconds to the time string
-    timeString += `:${seconds < 10 ? "0" + seconds : seconds}`;
-  }
-
-  return timeString;
-}
+  return minutes + ":" + seconds;
+};
 
 export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
   const sidebarOpen = useRecoilValue(sidebarOpenAtom);
@@ -62,7 +39,7 @@ export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
     timeTileShowingSecondsSelector(tileId)
   ) as [boolean | undefined, SetterOrUpdater<boolean | undefined>];
 
-  const intervalRef = useRef<number>();
+  const intervalRef = useRef<NodeJS.Timeout>();
   const color = `var(--text-color-${tileId})`;
 
   // old clients before the setting update need the value initialized
@@ -77,64 +54,116 @@ export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
   }
 
   const updateTime = React.useCallback(() => {
-    setTime(getCurrentTime(is12HourFormat!, isShowingSeconds!));
+    const now = new Date();
+
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    let timeString = "";
+
+    if (is12HourFormat) {
+      // Convert 24 hour time to 12 hour format
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      timeString = `${hours < 10 ? "0" + hours : hours}:${
+        minutes < 10 ? "0" + minutes : minutes
+      }`;
+    } else {
+      // 24 hour format
+      timeString = `${hours < 10 ? "0" + hours : hours}:${
+        minutes < 10 ? "0" + minutes : minutes
+      }`;
+    }
+
+    if (isShowingSeconds) {
+      // Append seconds to the time string
+      timeString += `:${seconds < 10 ? "0" + seconds : seconds}`;
+    }
+
+    setTime(timeString);
   }, [is12HourFormat, isShowingSeconds]);
 
   useEffect(() => {
+    updateTime();
     const timerId = setInterval(updateTime, 1000);
     return () => clearInterval(timerId);
-  }, [updateTime]);
+  }, [updateTime, is12HourFormat, isShowingSeconds]);
 
+  // Load timer state from localStorage
   useEffect(() => {
-    if (timer === 0) {
-      alert("Timer finished ⏱️");
-      setTimer(undefined);
-      clearInterval(intervalRef.current!);
-      document.title = "Timer finished!";
-    }
-  }, [timer]);
+    const loadTimer = () => {
+      const storedTimer = localStorage.getItem("timer");
+      const storedStartTime = localStorage.getItem("timer-start");
+      const storedDuration = localStorage.getItem("timer-duration");
+      if (storedTimer && storedStartTime && storedDuration) {
+        const now = Date.now();
+        const startTime = parseInt(storedStartTime, 10);
+        const duration = parseInt(storedDuration, 10);
+        const elapsedTime = Math.floor((now - startTime) / 1000);
+        const remainingTime = duration - elapsedTime;
 
-  const tickTimer = () => {
-    setTimer((prevTimer) => {
-      document.title = getTime(prevTimer! - 1);
-      return prevTimer! - 1;
-    });
-  };
+        if (remainingTime > 0) {
+          clearInterval(intervalRef.current);
+          setTimer(remainingTime);
+          startTimer(remainingTime); // Adjust this to resume from remaining time
+        } else {
+          clearTimerState();
+          alert("Timer finished!");
+        }
+      }
+    };
+    updateTime();
+    loadTimer();
 
-  const startOrResetTimer = () => {
-    if (intervalRef.current) {
+    return () => {
+      // Cleanup: Clear the interval when the component is unmounted
       clearInterval(intervalRef.current);
-    }
-    setTimer(timerPlaceholder! * 60);
-    setTimer(timerPlaceholder! * 60);
-    document.title = getTime(timerPlaceholder! * 60);
-    // will only run on the browser, so we can cast it
-    const interval = setInterval(tickTimer, 1000) as unknown as number;
-    intervalRef.current = interval;
+    };
+  }, []);
+
+  const startTimer = (duration: number) => {
+    setTimer(duration);
+    const startTime = Date.now();
+    localStorage.setItem("timer", duration.toString());
+    localStorage.setItem("timer-start", startTime.toString());
+    localStorage.setItem("timer-duration", duration.toString());
+
+    const intervalId = setInterval(() => {
+      const now = Date.now();
+      const elapsedTime = Math.floor((now - startTime) / 1000);
+      const remainingTime = duration - elapsedTime;
+
+      if (remainingTime >= 0) {
+        setTimer(remainingTime);
+      } else {
+        clearInterval(intervalId);
+        alert("Timer finished!");
+        clearTimerState();
+      }
+    }, 1000);
+
+    intervalRef.current = intervalId;
+
+    return () => clearInterval(intervalId);
   };
 
-  const getTime = (time: number) => {
-    let minutes: any = parseInt((time / 60) as unknown as string, 10);
-    let seconds: any = parseInt((time % 60) as unknown as string, 10);
-
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    if (isShowingSeconds) {
-      console.log("there");
-
-      return minutes + ":" + seconds;
-    } else {
-      console.log("here");
-
-      return minutes;
-    }
+  const handleStart = () => {
+    clearInterval(intervalRef.current);
+    const durationInSeconds = timerPlaceholder! * 60; // Convert minutes to seconds
+    startTimer(durationInSeconds);
   };
 
   const keyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
-      startOrResetTimer();
+      handleStart();
     }
+  };
+
+  const clearTimerState = () => {
+    setTimer(undefined);
+    localStorage.removeItem("timer");
+    localStorage.removeItem("timer-start");
+    localStorage.removeItem("timer-duration");
   };
 
   return (
@@ -149,7 +178,7 @@ export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
       position="relative"
     >
       <Heading marginX="auto" fontSize={isShowingTimer ? "38" : "42"}>
-        {timer ? getTime(timer) : time}
+        {timer ? getTimerDisplayText(timer) : time}
       </Heading>
       {isShowingTimer && (
         <Box display="flex" flexDir="row" width="200px" mt="3">
@@ -158,7 +187,7 @@ export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
             type="number"
             value={timerPlaceholder}
             onChange={(e) => setTimerPlaceholder(parseInt(e.target.value))}
-            placeholder="timer"
+            placeholder="timer - minutes"
             colorScheme="white"
             mb="4"
             mr="1"
@@ -177,7 +206,7 @@ export const TimeTile: React.FC<TimeProps> = ({ tileId }) => {
             ml="1"
             borderColor={color}
             borderWidth="1px"
-            onClick={startOrResetTimer}
+            onClick={handleStart}
           >
             {intervalRef.current ? "reset" : "go"}
           </OutlinedButton>
