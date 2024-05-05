7efe060f48822119a89642ff1f06ff6c47434c0e
Allister Grange <allistergrange@gmail.com>
2024-05-05 18:29:07 +1200

    Fixing build warnings for @magniquick

--- next.config.js
+++ next.config.js
@@ -6,10 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  env: {
-    API_URL: process.env.API_URL,
-    STRAVA_CLIENT: process.env.STRAVA_CLIENT,
-  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
