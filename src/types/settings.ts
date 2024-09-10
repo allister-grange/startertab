eb05b92aacb934059084139bbfdabf6c9c6f5ef3
Allister Grange <allistergrange@gmail.com>
2024-09-10 19:53:52 +1200

    Changing usingExternalCalendarForDayPlanner to be optional - it doesn't need to be mandatory (build breaking)

--- src/types/settings.ts
+++ src/types/settings.ts
@@ -132,7 +132,7 @@ export type TileSettings = {
   timeTileShowingTimer?: boolean;
   todoList?: TodoObject[];
   twitterFeedURL?: string;
-  usingExternalCalendarForDayPlanner: boolean;
+  usingExternalCalendarForDayPlanner?: boolean;
 };
 
 export type ThemeSettings = {
